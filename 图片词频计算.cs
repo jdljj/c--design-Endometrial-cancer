a7a027679585e16606082c346da5ab2ebc31338f
jdljj <69450972+jdljj@users.noreply.github.com>
2020-12-25 22:09:29 +0800

    Add files via upload

---
+++ 图片词频计算.cs
@@ -0,0 +1,134 @@
+﻿using JiebaNet.Segmenter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace 图云词频计算
+{
+    class Program
+    {
+        private static List<string> need_words;
+        private static double doc_num = 0;
+        private static int year;
+        static void Main(string[] args)
+        {
+            year = 1990;
+            string path = @"D:\c#\图云词频计算\need_words.txt";   //路径
+            string str = File.ReadAllText(path);
+            need_words = str.Split('\n').ToList<string>();    //将需要的单词添加
+            while (year <= 2020)
+            {
+                doc_num = 0;
+                method2();
+                words_rate();
+                year++;
+            }
+        }
+        public static void method2()
+        {
+            string path = @"D:\c#\stopwords-master\baidu_stopwords.txt";   //路径
+            string str = File.ReadAllText(path);
+            var stop_words = str.Split('\n');
+            path = @"D:\c#\图云词频计算\"+year;  //文件夹位置
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (FileInfo f in root.GetFiles())
+            {
+                doc_num++;
+                string fullName = f.FullName;
+                var text = File.ReadAllText(fullName);
+                string pattern = @"abstract:[\S\s]+";
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(text);
+                if (match.Groups.Count != 0)
+                {
+                    if (match.Groups[0].ToString().Length >= 9)
+                        text = match.Groups[0].ToString().Substring(9);
+                    else
+                        text = "abstract";
+                }
+                else
+                {
+                    text = "abstract"; //防止出现“”
+                }
+                if (text == "") text = "abstract";
+                var segmenter = new JiebaSegmenter();
+                var segments = segmenter.Cut(text, cutAll: true);  // 默认为精确模式
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\c#\图云词频计算\"+year+"words.txt", true);  //不覆盖
+                foreach (var temp in segments)
+                {
+                    if (!stop_words.Contains(temp))
+                    {
+                        file.WriteLine(temp);
+                    }
+                }
+                file.Close();
+            }
+        }
+        public static void words_rate()
+        {
+            string path = @"D:\c#\图云词频计算\words_rate.txt";   //路径
+            var rateLines = File.ReadAllLines(path);   //保存了原来words_rate.txt的每一行
+            string[] newrateLines = new string[rateLines.Length];
+
+            foreach (var temp in rateLines)
+            {
+                Console.WriteLine(temp);
+            }
+
+            List<string> AllWords = new List<string>();
+            List<int> AllFrequencies = new List<int>();
+            var file_words = File.ReadAllText(@"D:\c#\图云词频计算\"+year+"words.txt");
+            var words = file_words.Split('\n');
+            foreach (var word in words)
+            {
+                if (AllWords.Contains(word))
+                {
+                    //如果已经存在就+1
+                    AllFrequencies[AllWords.IndexOf(word)]++;
+                }
+                else
+                {
+                    bool result = false;
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (Char.IsNumber(word, j))
+                        {
+                            result = true;
+                        }
+                    }
+                    if (!result)
+                    {
+                        //如果不存在 且不为数字就添加
+                        AllWords.Add(word);
+                        AllFrequencies.Add(1);
+                    }
+                }
+            }
+            int index = 0;
+            foreach (var temp in AllFrequencies)
+            {
+                if (need_words.Contains(AllWords[index]))
+                {
+                    Console.WriteLine(need_words.IndexOf(AllWords[index]));
+                    newrateLines[need_words.IndexOf(AllWords[index]) + 1] = (double)1000*temp/doc_num + " " + rateLines[need_words.IndexOf(AllWords[index]) + 1];
+                }
+                index++;
+            }
+            System.IO.StreamWriter fileio = new System.IO.StreamWriter(@"D:\c#\图云词频计算\words_rate.txt", false);  //覆盖
+            newrateLines[0] = year+" " + rateLines[0];
+            foreach (var temp in newrateLines)
+            {
+                if (temp != "")
+                {
+                    Console.WriteLine(temp);
+                    fileio.WriteLine(temp);
+                }
+            }
+            fileio.Close();
+
+        }
+    }
+}
---
+++ 子宫内膜爬虫.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//https://www.jb51.net/article/177025.htm
+// \u0022双引号 \s{1,}多个空格 \S+任意非空格 [^\n]一切非换行符 [\s\t\n]+多个空格或换行
+// @"(?<=<div>)[\s\S]*?(?=</div>)" 提取两个div中的内容
+
+
+namespace 子宫内膜爬虫
+{
+    class Program
+    {
+        public static string HttpGet(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+        public static void GetInfomation(string html, int size, string year)
+        {
+            string pattern = @"<a\n\s{1,}class=\u0022docsum-title\u0022\n\s{1,}href=\u0022/(\d+)/\u0022\n\s{1,}ref=\u0022\S+\u0022\n\s{1,}data-ga-category=\u0022result_click\u0022";
+            pattern += @"\s{1,}data-ga-action=\u0022\S+\u0022\n\s{1,}data-ga-label=\u0022\S+\u0022\n\s{1,}data-full-article-url=\u0022\S+\u0022\n\s{1,}data-article-id=\u0022\d+\u0022>\n";
+            pattern += @"\s{1,}([^\n]+)\n\s{1,}</a>";
+            MatchCollection matches = Regex.Matches(html, pattern);
+            string pattern2 = @"<span class=\u0022docsum-journal-citation full-journal-citation\u0022>([^.]+). ([^;]+);";
+            MatchCollection matches2 = Regex.Matches(html, pattern2);
+            for (int i = 0; i < size; i++)
+            {
+                if (matches[i] == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+                String filename = "files/" + year + "/" + matches[i].Groups[1] + ".txt";
+                StreamWriter sw = new StreamWriter(filename)
+                {
+                    AutoFlush = true
+                };
+                String url = "https://pubmed.ncbi.nlm.nih.gov/" + matches[i].Groups[1] + "/";
+                sw.WriteLine("url=" + url);
+                String title = matches[i].Groups[2].ToString();
+                title = title.Replace("<b>", "");
+                title = title.Replace("</b>", "");
+                sw.WriteLine("title=" + title);
+                sw.WriteLine("from:" + matches2[i].Groups[1]);
+                sw.WriteLine("time=" + matches2[i].Groups[2]);
+                String abs_html = HttpGet(url, "");
+                String abstr = GetAbstract(abs_html);
+                sw.Write("abstract: " + abstr);
+            }
+        }
+
+        static String GetAbstract(string html)
+        {
+            string pattern = @"(?<=<div class=\u0022abstract-content selected\u0022\n\s{1,}id=\u0022enc-abstract\u0022>)[\s\S]*?(?=</div>)";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(html);
+            string result = match.Groups[0].ToString();
+            result = result.Replace("\n", "");
+            result = result.Replace("<p>", "");
+            result = result.Replace("</p>", "");
+            result = result.Replace("<strong class=\"sub-title\">", "");
+            result = result.Replace("</strong>", "");
+            result = result.Replace("  ", "");
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string year = "2000";
+            int index = 1;
+            while (true)
+            {
+                int page = index;
+                String info = HttpGet("https://pubmed.ncbi.nlm.nih.gov/?term=endometrial%20cancer&filter=years." + year + "-" + year + "&size=200&page=" + page, "");
+                GetInfomation(info, 200, year);
+                index++;
+            }
+        }
+    }
+}
